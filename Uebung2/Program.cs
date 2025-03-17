41006b130743bb860e7b98ef5b73588570865fae
levbeck <levin.beck@mercedes-benz.com>
2025-03-17 14:29:35 +0100

    Finished Uebung2

---
+++ Uebung2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Uebung2;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 0; i > -1; i+=2)
+        { 
+            Console.WriteLine(i);
+        }
+    }
+}
